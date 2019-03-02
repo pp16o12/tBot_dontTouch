1b2a64d9e76a525b4d46aa7a1c9c3812a9bb59a7
student <student@104-05>
2019-03-02 10:09:10 +0200

    add welcome message on start

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,8 +9,30 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static TelegramBotClient botClient;
         static void Main(string[] args)
         {
+            botClient = new TelegramBotClient("742631594:AAE5uxvJTix99mbS6-HK3WpKS8ndAvaIGk4");
+            var bot = botClient.GetMeAsync().Result;
+            Console.WriteLine(bot.Username);
+
+            botClient.OnMessage += BotClient_OnMessage;
+            botClient.StartReceiving();
+            Console.ReadLine();
+        }
+
+        private static void BotClient_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            Console.WriteLine(e.Message.Chat.Id + ":" + e.Message.Chat.Username + "= " + e.Message.Text);
+            switch (e.Message.Text)
+            {
+                case "/start":
+                    botClient.SendTextMessageAsync(e.Message.Chat.Id, "Welcome");
+                    //botClient.SendPhotoAsync(e.Message.Chat.Id, "C:/Users/student/Desktop/83234881-medieval-game-sprites-characters-collection-arbalester.jpg");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
